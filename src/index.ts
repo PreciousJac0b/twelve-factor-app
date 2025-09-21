4b8da40b82ee463ed5f3ba0477cfbbfd368c93dd
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2025-09-21 16:42:58 +0100

    Added CORS

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app: Application = express();
 
 const allowedOrigins = [
-  'http://localhost:3000',            
+  'http://localhost:5173',            
   'https://time-zone-app.vercel.app/'
 ];
 
